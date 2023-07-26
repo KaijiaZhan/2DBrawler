b4c8ccfd6a95109c2783b7cef33af0d2ebce7a34
KaijiaZhan <zhanjoan@msu.edu>
2023-07-26 16:04:55 -0400

    Added PlayerDuck
    Added idle animation, walking animation, and added the jumping sprite but still need to implement the animation

--- My project/Assets/Scripts/PlayerMovement.cs
+++ My project/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,7 @@ public class PlayerMovement : MonoBehaviour
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
+    private Animator anim;
 
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
@@ -16,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -27,6 +29,18 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
+        if (horizontal > 0f)
+        {
+            anim.SetBool("Running", true);
+        }
+        else if (horizontal < 0f) 
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
         Flip();
     }
 
