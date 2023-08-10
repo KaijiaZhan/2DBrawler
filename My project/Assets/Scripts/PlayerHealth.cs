37e0b6887a26dea93ed7d5744fdb82006ba4e319
KaijiaZhan <zhanjoan@msu.edu>
2023-08-09 20:21:56 -0400

    Added health bar image

--- My project/Assets/Scripts/PlayerHealth.cs
+++ My project/Assets/Scripts/PlayerHealth.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
-    [SerializeField] private float health;
+    //[SerializeField] private float health;
     private Rigidbody2D rb;
 
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBarScripti healthBar;
+
     // Start is called before the first frame update
-    private void Start()
+    void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.CompareTag("Trap")) {
-            health -= 5;
-            if (health <= 0) {
+            currentHealth -= 5;
+            if (currentHealth <= 0) {
                 Destroy(gameObject);
             }
         }
